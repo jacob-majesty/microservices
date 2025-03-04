1777a98e37da0e97a220716ec162778dde80125d
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-04 15:33:49 -0300

    Feat: Add files to entity

---
+++ accounts/src/main/java/com/majesty/accounts/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.majesty.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="customer_id")
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+
+    @Column(name="mobile_number")
+    private String mobileNumber;
+
+}
