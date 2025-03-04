d0a10f0b7f403cfa7129c72344d2783b8d139b62
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-04 15:40:42 -0300

    Feat: Add Accounts and Customer to repository

---
+++ accounts/src/main/java/com/majesty/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.majesty.accounts.repository;
+
+import com.majesty.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
