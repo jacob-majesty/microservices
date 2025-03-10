6cbc36147be37118f54b810766704c9f3b34dfd5
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-09 21:52:23 -0300

    Add Customer service

---
+++ version4/accounts/src/main/java/com/majesty/accounts/service/ICustomersService.java
@@ -0,0 +1,13 @@
+package com.majesty.accounts.service;
+
+import com.majesty.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomersService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
