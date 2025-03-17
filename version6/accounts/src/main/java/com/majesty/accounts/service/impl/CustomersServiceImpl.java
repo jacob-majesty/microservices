0e1474bbf9129b00e8ba0da97cf898247300f5fe
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-03-17 02:12:14 -0300

    v6: Initial commit[v5]

---
+++ version6/accounts/src/main/java/com/majesty/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,56 @@
+package com.majesty.accounts.service.impl;
+
+import com.majesty.accounts.dto.AccountsDto;
+import com.majesty.accounts.dto.CardsDto;
+import com.majesty.accounts.dto.CustomerDetailsDto;
+import com.majesty.accounts.dto.LoansDto;
+import com.majesty.accounts.entity.Accounts;
+import com.majesty.accounts.entity.Customer;
+import com.majesty.accounts.exception.ResourceNotFoundException;
+import com.majesty.accounts.mapper.AccountsMapper;
+import com.majesty.accounts.mapper.CustomerMapper;
+import com.majesty.accounts.repository.AccountsRepository;
+import com.majesty.accounts.repository.CustomerRepository;
+import com.majesty.accounts.service.ICustomersService;
+import com.majesty.accounts.service.client.CardsFeignClient;
+import com.majesty.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     *  @param correlationId - Correlation ID value generated at Edge server
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+
+    }
+}
